40e6ec12ce003eacd6c391b5b9a26e1d551a07c2
George <g.bisbas18@imperial.ac.uk>
2020-03-09 12:12:15 +0200

    Init a backdoor c file

--- examples/cfd/laplacian_test.py
+++ examples/cfd/laplacian_test.py
@@ -10,21 +10,23 @@ configuration['dle'] = 'advanced'
 # Some variable declarations
 nx = 81
 ny = 81
+nz = 81
 nt = 100
 c = 1.
 dx = 2. / (nx - 1)
 dy = 2. / (ny - 1)
-print("dx %s, dy %s" % (dx, dy))
+dz = 2. / (nz - 1)
+print("dx %s, dy %s, dz %s" % (dx, dy, dz))
 sigma = .2
 dt = sigma * dx
 
-grid = Grid(shape=(nx, ny), extent=(2., 2.))
+grid = Grid(shape=(nx, ny, nz), extent=(2., 2., 2.))
 u = TimeFunction(name='u', grid=grid, time_order=2, space_order=8)
 init_hat(field=u.data[0], dx=dx, dy=dy, value=2.)
 
 pde = u.laplace
 
-op = Operator(Eq(u.forward, pde))
+#op = Operator(Eq(u.forward, pde))
 
 # print(op.ccode)
 
@@ -42,7 +44,7 @@ f0 = 0.010  # Source peak frequency is 10Hz (0.010 kHz)
 src = RickerSource(name='src', grid=grid, f0=f0,
                    npoint=1, time_range=time_range)
 
-domain_size = (1000.0, 1000.0)
+domain_size = (1000.0, 1000.0, 1000.0)
 
 # First, position source centrally in all dimensions, then set depth
 src.coordinates.data[0, :] = np.array(domain_size) * .5
@@ -61,10 +63,7 @@ op = Operator([Eq(u.forward, pde)] + src_term)
 
 # test = op.ccode
 
-op(time=2000)
+op(time=100)
 
 print('--------------------------------------')
-print(type(op.ccode))
-
-
-import pdb; pdb.set_trace()
+#print(type(op.ccode))
