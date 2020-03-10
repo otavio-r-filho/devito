989aa9cf5c91c07fd58d0adbac3f2441f2d74215
George <g.bisbas18@imperial.ac.uk>
2020-03-10 14:55:29 +0200

    Passing-to add inner z, to add header files

--- examples/cfd/tt_poc.c
+++ examples/cfd/tt_poc.c
@@ -26,6 +26,92 @@ int *malloc3d_and_init_int_cont(int dim1, int dim2, int dim3, int value)
   return p;
 }
 
+// Function to allocate memory for u[t][x][y]
+int ***malloc_3d_int(int dim1, int dim2, int dim3)
+{
+  //  printf("\nAllocating 3d...");
+  int ***matrix3d;
+
+  matrix3d = (int ***)malloc(dim1 * sizeof(int **));
+  for (int idim1 = 0; idim1 < dim1; idim1++)
+  {
+    matrix3d[idim1] = (int **)malloc(dim2 * sizeof(int *));
+    for (int idim2 = 0; idim2 < dim2; idim2++)
+    {
+      matrix3d[idim1][idim2] = (int *)malloc(dim3 * sizeof(int));
+    }
+  }
+  //printf("...DONE\n");
+  return matrix3d;
+}
+
+// Function to allocate memory for u[t][x][y]
+void initialize3_int(int timestamps, int nrows, int ncols, int ***A_init, int value)
+{
+  // Initialize the matrix
+  //printf("\nInit 3d...");
+  for (int ti = 0; ti < timestamps; ti++)
+  {
+    for (int xi = 0; xi < nrows; xi++)
+    {
+      for (int yi = 0; yi < ncols; yi++)
+      {
+        A_init[ti][xi][yi] = value; //2 * xi - 1.4 * yi + 0.5 * fabs(xi - yi);
+      }
+    }
+  }
+  //printf("...DONE\n");
+}
+
+// Function to allocate memory for u[t][x][y]
+void initialize2(int nrows, int ncols, float **A_init, float value)
+{
+  // Initialize the matrix
+  //printf("\nInit 3d...");
+  int xi = 0;
+  int yi = 0;
+
+  for (xi = 0; xi < nrows; xi += 1)
+  {
+    for (yi = 0; yi < ncols; yi += 1)
+    {
+      A_init[xi][yi] = value; //2 * xi - 1.4 * yi + 0.5 * fabs(xi - yi);
+    }
+  }
+  //printf("...DONE\n");
+}
+
+int **malloc_2d_int(int dim1, int dim2)
+{
+  //  printf("\nAllocating 3d...");
+  int **matrix2d;
+
+  matrix2d = (int **)malloc(dim1 * sizeof(int *));
+
+  for (int idim1 = 0; idim1 < dim1; idim1++)
+  {
+    matrix2d[idim1] = (int *)malloc(dim2 * sizeof(int));
+  }
+  //printf("...DONE\n");
+  return matrix2d;
+}
+
+float **malloc_2d_float(int dim1, int dim2)
+{
+  //  printf("\nAllocating 3d...");
+  float **matrix2d;
+
+  matrix2d = (float **)malloc(dim1 * sizeof(float *));
+
+  for (int idim1 = 0; idim1 < dim1; idim1++)
+  {
+    matrix2d[idim1] = (float *)malloc(dim2 * sizeof(float));
+  }
+  //printf("...DONE\n");
+  return matrix2d;
+}
+
+
 struct dataobj
 {
   void *restrict data;
@@ -77,17 +163,16 @@ int Kernel(const float h_x, const float h_y, const float h_z, const float o_x, c
         int ii_src_3 = (int)(floor((-o_z + src_coords[p_src][2]) / h_z)) + 1;
         int ii_src_4 = (int)(floor((-o_y + src_coords[p_src][1]) / h_y)) + 1;
         int ii_src_5 = (int)(floor((-o_x + src_coords[p_src][0]) / h_x)) + 1;
-        float px = (float)(-h_x * (int)(floor((-o_x + src_coords[p_src][0]) / h_x)) - o_x + src_coords[p_src][0]);
-        float py = (float)(-h_y * (int)(floor((-o_y + src_coords[p_src][1]) / h_y)) - o_y + src_coords[p_src][1]);
-        float pz = (float)(-h_z * (int)(floor((-o_z + src_coords[p_src][2]) / h_z)) - o_z + src_coords[p_src][2]);
+        //float px = (float)(-h_x * (int)(floor((-o_x + src_coords[p_src][0]) / h_x)) - o_x + src_coords[p_src][0]);
+        //float py = (float)(-h_y * (int)(floor((-o_y + src_coords[p_src][1]) / h_y)) - o_y + src_coords[p_src][1]);
+        //float pz = (float)(-h_z * (int)(floor((-o_z + src_coords[p_src][2]) / h_z)) - o_z + src_coords[p_src][2]);
         if (ii_src_0 >= x_m - 1 && ii_src_1 >= y_m - 1 && ii_src_2 >= z_m - 1 && ii_src_0 <= x_M + 1 && ii_src_1 <= y_M + 1 && ii_src_2 <= z_M + 1)
         {
-            if (source_mask[ii_src_0 + 8][ii_src_1 + 8][ii_src_2 + 8] == 0)
-            {
-              source_id[ii_src_0 + 8][ii_src_1 + 8][ii_src_2 + 8] = id++;
-              source_mask[ii_src_0 + 8][ii_src_1 + 8][ii_src_2 + 8] = 1;
-            }
-          
+          if (source_mask[ii_src_0 + 8][ii_src_1 + 8][ii_src_2 + 8] == 0)
+          {
+            source_id[ii_src_0 + 8][ii_src_1 + 8][ii_src_2 + 8] = id++;
+            source_mask[ii_src_0 + 8][ii_src_1 + 8][ii_src_2 + 8] = 1;
+          }
         }
         if (ii_src_0 >= x_m - 1 && ii_src_1 >= y_m - 1 && ii_src_3 >= z_m - 1 && ii_src_0 <= x_M + 1 && ii_src_1 <= y_M + 1 && ii_src_3 <= z_M + 1)
         {
@@ -189,27 +274,91 @@ int Kernel(const float h_x, const float h_y, const float h_z, const float o_x, c
     }
   }
 
-// Sparse structs are built here
-
-
+  // Sparse structs are already built here
 
   float **save_src;
   save_src = malloc_2d_float(id, time_M);
   initialize2(id, time_M, save_src, 0.0F);
 
-  for (int ti = 0; ti < time_M; ti++)
+  for (int time = time_m; time <= time_M; time += 1)
   {
-    for (int p_src = 0; p_src < p_src_M; p_src++)
+    /* Begin section1 */
     {
-      //printf("\n Validate source: %d", p_src);
-
-      //xx = src_coords[p_src][0];
-      //yy = src_coords[p_src][1];
-      //zz = src_coords[p_src][2];
-      //printf("\n B Saved src is with : %d, %d, %d, %f ti = %d", xx, yy, zz, save_src[source_id[xx][yy][zz]][ti], ti);
-      //save_src[(source_id[xx][yy][zz])][ti] += src[ti][p_src];
-      //printf("\n A Saved src is with : %d, %d, %d, %f ti = %d", xx, yy, zz, save_src[source_id[xx][yy][zz]][ti], ti);
+      int chunk_size = (int)(fmax(1, (1.0F / 3.0F) * (p_src_M - p_src_m + 1) / nthreads_nonaffine));
+      for (int p_src = p_src_m; p_src <= p_src_M; p_src += 1)
+      {
+        int ii_src_0 = (int)(floor((-o_x + src_coords[p_src][0]) / h_x));
+        int ii_src_1 = (int)(floor((-o_y + src_coords[p_src][1]) / h_y));
+        int ii_src_2 = (int)(floor((-o_z + src_coords[p_src][2]) / h_z));
+        int ii_src_3 = (int)(floor((-o_z + src_coords[p_src][2]) / h_z)) + 1;
+        int ii_src_4 = (int)(floor((-o_y + src_coords[p_src][1]) / h_y)) + 1;
+        int ii_src_5 = (int)(floor((-o_x + src_coords[p_src][0]) / h_x)) + 1;
+        float px = (float)(-h_x * (int)(floor((-o_x + src_coords[p_src][0]) / h_x)) - o_x + src_coords[p_src][0]);
+        float py = (float)(-h_y * (int)(floor((-o_y + src_coords[p_src][1]) / h_y)) - o_y + src_coords[p_src][1]);
+        float pz = (float)(-h_z * (int)(floor((-o_z + src_coords[p_src][2]) / h_z)) - o_z + src_coords[p_src][2]);
+        if (ii_src_0 >= x_m - 1 && ii_src_1 >= y_m - 1 && ii_src_2 >= z_m - 1 && ii_src_0 <= x_M + 1 && ii_src_1 <= y_M + 1 && ii_src_2 <= z_M + 1)
+        {
+          float r3 = 1.0e-4F * (-px * py * pz / (h_x * h_y * h_z) + px * py / (h_x * h_y) + px * pz / (h_x * h_z) - px / h_x + py * pz / (h_y * h_z) - py / h_y - pz / h_z + 1) * src[time][p_src];
+          //u[t1][ii_src_0 + 8][ii_src_1 + 8][ii_src_2 + 8] += r3;
+          save_src[(source_id[ii_src_0 + 8][ii_src_1 + 8][ii_src_2 + 8])][time] += r3;
+        }
+        if (ii_src_0 >= x_m - 1 && ii_src_1 >= y_m - 1 && ii_src_3 >= z_m - 1 && ii_src_0 <= x_M + 1 && ii_src_1 <= y_M + 1 && ii_src_3 <= z_M + 1)
+        {
+          float r4 = 1.0e-4F * (px * py * pz / (h_x * h_y * h_z) - px * pz / (h_x * h_z) - py * pz / (h_y * h_z) + pz / h_z) * src[time][p_src];
+          //u[t1][ii_src_0 + 8][ii_src_1 + 8][ii_src_3 + 8] += r4;
+          save_src[(source_id[ii_src_0 + 8][ii_src_1 + 8][ii_src_3 + 8])][time] += r4;
+        }
+        if (ii_src_0 >= x_m - 1 && ii_src_2 >= z_m - 1 && ii_src_4 >= y_m - 1 && ii_src_0 <= x_M + 1 && ii_src_2 <= z_M + 1 && ii_src_4 <= y_M + 1)
+        {
+          float r5 = 1.0e-4F * (px * py * pz / (h_x * h_y * h_z) - px * py / (h_x * h_y) - py * pz / (h_y * h_z) + py / h_y) * src[time][p_src];
+          //u[t1][ii_src_0 + 8][ii_src_4 + 8][ii_src_2 + 8] += r5;
+          save_src[(source_id[ii_src_0 + 8][ii_src_4 + 8][ii_src_2 + 8])][time] += r5;
+        }
+        if (ii_src_0 >= x_m - 1 && ii_src_3 >= z_m - 1 && ii_src_4 >= y_m - 1 && ii_src_0 <= x_M + 1 && ii_src_3 <= z_M + 1 && ii_src_4 <= y_M + 1)
+        {
+          float r6 = 1.0e-4F * (-px * py * pz / (h_x * h_y * h_z) + py * pz / (h_y * h_z)) * src[time][p_src];
+          //u[t1][ii_src_0 + 8][ii_src_4 + 8][ii_src_3 + 8] += r6;
+          save_src[(source_id[ii_src_0 + 8][ii_src_4 + 8][ii_src_3 + 8])][time] += r6;
+        }
+        if (ii_src_1 >= y_m - 1 && ii_src_2 >= z_m - 1 && ii_src_5 >= x_m - 1 && ii_src_1 <= y_M + 1 && ii_src_2 <= z_M + 1 && ii_src_5 <= x_M + 1)
+        {
+          float r7 = 1.0e-4F * (px * py * pz / (h_x * h_y * h_z) - px * py / (h_x * h_y) - px * pz / (h_x * h_z) + px / h_x) * src[time][p_src];
+          //u[t1][ii_src_5 + 8][ii_src_1 + 8][ii_src_2 + 8] += r7;
+          save_src[(source_id[ii_src_5 + 8][ii_src_1 + 8][ii_src_2 + 8])][time] += r7;
+        }
+        if (ii_src_1 >= y_m - 1 && ii_src_3 >= z_m - 1 && ii_src_5 >= x_m - 1 && ii_src_1 <= y_M + 1 && ii_src_3 <= z_M + 1 && ii_src_5 <= x_M + 1)
+        {
+          float r8 = 1.0e-4F * (-px * py * pz / (h_x * h_y * h_z) + px * pz / (h_x * h_z)) * src[time][p_src];
+          //u[t1][ii_src_5 + 8][ii_src_1 + 8][ii_src_3 + 8] += r8;
+          save_src[(source_id[ii_src_5 + 8][ii_src_1 + 8][ii_src_3 + 8])][time] += r8;
+        }
+        if (ii_src_2 >= z_m - 1 && ii_src_4 >= y_m - 1 && ii_src_5 >= x_m - 1 && ii_src_2 <= z_M + 1 && ii_src_4 <= y_M + 1 && ii_src_5 <= x_M + 1)
+        {
+          float r9 = 1.0e-4F * (-px * py * pz / (h_x * h_y * h_z) + px * py / (h_x * h_y)) * src[time][p_src];
+          //u[t1][ii_src_5 + 8][ii_src_4 + 8][ii_src_2 + 8] += r9;
+          save_src[(source_id[ii_src_5 + 8][ii_src_4 + 8][ii_src_2 + 8])][time] += r9;
+        }
+        if (ii_src_3 >= z_m - 1 && ii_src_4 >= y_m - 1 && ii_src_5 >= x_m - 1 && ii_src_3 <= z_M + 1 && ii_src_4 <= y_M + 1 && ii_src_5 <= x_M + 1)
+        {
+          float r10 = 1.0e-4F * px * py * pz * src[time][p_src] / (h_x * h_y * h_z);
+          //u[t1][ii_src_5 + 8][ii_src_4 + 8][ii_src_3 + 8] += r10;
+          save_src[(source_id[ii_src_5 + 8][ii_src_4 + 8][ii_src_3 + 8])][time] += r10;
+        }
+      }
     }
+    /* End section1 */
+
+    //for (int p_src = 0; p_src < p_src_M; p_src++)
+    //{
+    //printf("\n Validate source: %d", p_src);
+
+    //xx = src_coords[p_src][0];
+    //yy = src_coords[p_src][1];
+    //zz = src_coords[p_src][2];
+    //printf("\n B Saved src is with : %d, %d, %d, %f ti = %d", xx, yy, zz, save_src[source_id[xx][yy][zz]][ti], ti);
+    //save_src[(source_id[xx][yy][zz])][ti] += src[ti][p_src];
+    //printf("\n A Saved src is with : %d, %d, %d, %f ti = %d", xx, yy, zz, save_src[source_id[xx][yy][zz]][ti], ti);
+    //}
   }
 
   for (int time = time_m, t0 = (time) % (3), t1 = (time + 1) % (3); time <= time_M; time += 1, t0 = (time) % (3), t1 = (time + 1) % (3))
@@ -227,6 +376,7 @@ int Kernel(const float h_x, const float h_y, const float h_z, const float o_x, c
     struct timeval start_section1, end_section1;
     gettimeofday(&start_section1, NULL);
 /* Begin section1 */
+/*
 #pragma omp parallel num_threads(nthreads_nonaffine)
     {
       int chunk_size = (int)(fmax(1, (1.0F / 3.0F) * (p_src_M - p_src_m + 1) / nthreads_nonaffine));
@@ -292,7 +442,8 @@ int Kernel(const float h_x, const float h_y, const float h_z, const float o_x, c
         }
       }
     }
-    /* End section1 */
+  */
+   /* End section1 */
     gettimeofday(&end_section1, NULL);
     timers->section1 += (double)(end_section1.tv_sec - start_section1.tv_sec) + (double)(end_section1.tv_usec - start_section1.tv_usec) / 1000000;
   }
@@ -317,6 +468,14 @@ void bf0(const float h_x, const float h_y, const float h_z, struct dataobj *rest
         {
           for (int y = y0_blk0; y <= y0_blk0 + y0_blk0_size - 1; y += 1)
           {
+#pragma omp simd aligned(u : 32)
+            for (int z = z_m; z <= z_M; z += 1)
+            {
+              float r2 = 1.0 / (h_z * h_z);
+              float r1 = 1.0 / (h_y * h_y);
+              float r0 = 1.0 / (h_x * h_x);
+              u[t1][x + 8][y + 8][z + 8] = r0 * (-1.78571429e-3F * (u[t0][x + 4][y + 8][z + 8] + u[t0][x + 12][y + 8][z + 8]) + 2.53968254e-2F * (u[t0][x + 5][y + 8][z + 8] + u[t0][x + 11][y + 8][z + 8]) - 2.0e-1F * (u[t0][x + 6][y + 8][z + 8] + u[t0][x + 10][y + 8][z + 8]) + 1.6F * (u[t0][x + 7][y + 8][z + 8] + u[t0][x + 9][y + 8][z + 8]) - 2.84722222F * u[t0][x + 8][y + 8][z + 8]) + r1 * (-1.78571429e-3F * (u[t0][x + 8][y + 4][z + 8] + u[t0][x + 8][y + 12][z + 8]) + 2.53968254e-2F * (u[t0][x + 8][y + 5][z + 8] + u[t0][x + 8][y + 11][z + 8]) - 2.0e-1F * (u[t0][x + 8][y + 6][z + 8] + u[t0][x + 8][y + 10][z + 8]) + 1.6F * (u[t0][x + 8][y + 7][z + 8] + u[t0][x + 8][y + 9][z + 8]) - 2.84722222F * u[t0][x + 8][y + 8][z + 8]) + r2 * (-1.78571429e-3F * (u[t0][x + 8][y + 8][z + 4] + u[t0][x + 8][y + 8][z + 12]) + 2.53968254e-2F * (u[t0][x + 8][y + 8][z + 5] + u[t0][x + 8][y + 8][z + 11]) - 2.0e-1F * (u[t0][x + 8][y + 8][z + 6] + u[t0][x + 8][y + 8][z + 10]) + 1.6F * (u[t0][x + 8][y + 8][z + 7] + u[t0][x + 8][y + 8][z + 9]) - 2.84722222F * u[t0][x + 8][y + 8][z + 8]);
+            }
 #pragma omp simd aligned(u : 32)
             for (int z = z_m; z <= z_M; z += 1)
             {
